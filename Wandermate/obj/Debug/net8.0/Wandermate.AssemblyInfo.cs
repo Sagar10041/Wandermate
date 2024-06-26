dcb7c7a72b2553f1e700c13824e2aea6d5534b2a
SagarBasnet <Sagar.basnet@iic.edu.np>
2024-06-26 09:56:13 +0545

    tesai

--- Wandermate/obj/Debug/net8.0/Wandermate.AssemblyInfo.cs
+++ Wandermate/obj/Debug/net8.0/Wandermate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wandermate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9d7302d4d87aaaa65fd069f229e93bf38f2bfef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bee60bd9b3ffd0db9777d6a16e070588b852cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wandermate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wandermate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
